236c3fb2829efe79a551a7e729d5259faceb8f13
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2024-10-13 22:17:56 +0200

    create the user model,the database using SQLALchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    #Load the app configurations 
+    app.config.from_object('app.config.config')
+
+    #initialise the database and the migration tools 
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    #Register the routes 
+    from app.routes import user_bp
+    app.register_blueprint(user_bp)
+
+    return app
+
+
